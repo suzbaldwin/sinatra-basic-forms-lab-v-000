7ae21dd017644ff0863660354a596d069492ee62
suzy baldwin <gbaldwin24@comcast.net>
2018-03-20 19:59:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,25 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "Click Here To List A Puppy"
+    erb :index
+
+  end
+
+  get '/welcome' do
+    "Click Here To List A Puppy"
+    erb :index
+  end
+  get '/new' do
+    erb :create_puppy
+  end
+  post '/' do
+    @name = "#{params[:name]}"
+    @breed = "#{params[:breed]}"
+    @age = "#{params[:age]}"
+    erb :display_puppy
+
+  end
+
 end

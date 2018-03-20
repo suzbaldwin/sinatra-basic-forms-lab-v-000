e77b56d86bdd1e116861b06d15f750b5b344dd69
suzy baldwin <gbaldwin24@comcast.net>
2018-03-20 19:23:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,26 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-get '/' do
-  "Click Here To List A Puppy"
-  erb :index
-
-end
-
-get '/welcome' do
-  "Click Here To List A Puppy"
-  erb :index
-end
-get '/new' do
-erb :create_puppy
-end
-post '/' do
-  @name = "Puppy Name: #{params[:name]}"
-  @breed = "Puppy Breed: #{params[:breed]}"
-  @age = "Puppy Age: #{params[:age]}"
-erb :display_puppy
-
-end
-
+  
+  get '/' do
+    "Click Here To List A Puppy"
+    erb :index
+    
+  end
+  
+  get '/welcome' do
+    "Click Here To List A Puppy"
+    erb :index
+  end
+  get '/new' do
+    erb :create_puppy
+  end
+  post '/' do
+    @name = "Puppy Name: #{params[:name]}"
+    @breed = "Puppy Breed: #{params[:breed]}"
+    @age = "Puppy Age: #{params[:age]}"
+    erb :display_puppy
+    
+  end
+  
 end
